5a7205d1af73c9b12cdcbec9b344e741bda4f33b
abhidivami <abhilashadunuri@divami.com>
2025-04-02 17:47:20 +0530

    Revert "Feature/notification page"

--- license-management/src/App.tsx
+++ license-management/src/App.tsx
@@ -1,37 +1,35 @@
-// In App component
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './Redux/Store';
-import './App.css';
-import { AgGridTable } from './common/component/AgGridTable/index';
-import Navbar from './common/components/Navbar';
-import Sidebar from './common/components/Navbar/Sidebar';
-import DetailedViewOfEachRecord from './common/DetailedViewOfRecord/index';
-import Analytics from './components/Analytics/index';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Navbar />
-        <div className="main">
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<AgGridTable page="home" />} />
-            <Route path="/home" element={<AgGridTable page="home"/>} />
-            <Route path="/detailedView" element={<DetailedViewOfEachRecord />} />
-            <Route path="/analytics" element={<Analytics />} />
-            <Route path="/expired" element={<AgGridTable page="expired" />} />
-          </Routes>
-          {/* <LicenseForm/>
-          <AgGridTable/> */}
-          <ToastContainer />
-        </div>
-      </BrowserRouter>
-    </Provider>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
