65371e9b3ed7d18a4c24a5357f241ca097d3dcb3
ravitejadivami <ravitejam@divami.com>
2025-04-09 13:41:42 +0530

    changes on notfication and responsiveness and db data changed

--- license-management/src/common/components/Navbar/Profile/index.tsx
+++ license-management/src/common/components/Navbar/Profile/index.tsx
@@ -7,19 +7,21 @@ import profileLogo from '../../../../assets/brand.jpg';
 import styles from './index.module.scss';
 import LogoutSharpIcon from '@mui/icons-material/LogoutSharp';
 import CloseIcon from '@mui/icons-material/CancelSharp';
-import logo from '../assets/logo.png';
 
-
-
+//to print color color icon based on screen size 
 interface ProfileProps {
     iconColor: "white" | "black";
 }
+
+
+
 function Profile(props: ProfileProps) {
-
+    //icon color props
     const { iconColor } = props;
-
+    //useState in mui Drawer
     const [open, setOpen] = React.useState(false);
-
+    
+    //open toggle drawer 
     const toggleDrawer = (newOpen: boolean) => () => {
         setOpen(newOpen);
     };
@@ -27,50 +29,42 @@ function Profile(props: ProfileProps) {
     //to style profile icon
     const menuId = 'primary-search-account-menu';
 
-
     const DrawerList = (
-        <Box sx={{ width: iconColor == "black" ? 320 : 400,margin:'10px', boxShadow: "0px 0px 6px rgba(0, 0, 0, 0.6)",borderRadius:"10px" , background:'white', height:"100vh",overflow:"scroll", "::-webkit-scrollbar":{display:"none"} }} role="Profile" onClick={toggleDrawer(false)}>
-          
+        //
+        <Box sx={{ width: iconColor == "black" ? 320 : 400, margin:'10px', boxShadow: "0px 0px 6px rgba(0, 0, 0, 0.6)",borderRadius:"10px" , background:'white', height:"100vh",overflow:"scroll", "::-webkit-scrollbar":{display:"none"} }} role="Profile" onClick={toggleDrawer(false)}>   
             <div className={styles.profile}>
-                <div style={{display:"flex"}}> 
-            <Card sx={{height:"130px",width:"100%", }}>
+              <div style={{display:"flex"}}>      
+               <Card sx={{height:"130px",width:"100%", }}> 
                 <CardContent>
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                       
+                    <div style={{ display: "flex", alignItems: "center" }}>      
                      <Avatar sx={{ width: "80px", height: "80px", borderRadius: "10px" }}  />
                         <div style={{ marginLeft: "10px" }}>
                             <p style={{fontWeight:"500"}}>Divami</p>
                             <p style={{opacity:"0.7"}}>Divami@gmail.com</p>
                         </div>
                     </div>
-                        <div style={{display:"flex",justifyContent:"end",gap:"10px",cursor:"pointer"}}>
-                             <LogoutSharpIcon sx={{color:"red"}} />
-                            <p>Sign Out</p>
-                        </div>
+                    <div style={{display:"flex",justifyContent:"end",gap:"10px",cursor:"pointer"}}>
+                            <LogoutSharpIcon sx={{color:"red"}} />
+                        <p>Sign Out</p>
+                    </div>
                 </CardContent>
-             </Card>
+               </Card>
 
              <div style={{display:"flex",justifyContent:"end",cursor:"pointer"}}>
                  <CloseIcon />
-               </div>
-                </div>
-             
+              </div>
+              </div>
               
                 <Card sx={{marginTop:"60px",height:"100px"}}>
                     <CardContent sx={{display:"flex", justifyContent:"center",alignItems:"center",gap:"10px"}}>
                       <img src={profileLogo} className={styles.profileLogo} />
                      <span className={styles.profileTitle}>Divami Design Labs Private Limited</span>
                     </CardContent>
-
-                </Card>
-              
+                </Card>   
             </div>
             <Divider />
-            <br/>
-          
-            <p className={styles.caption}>Focuses on design excellence and strategic growth</p>
-
-            
+            <br/>  
+            <p className={styles.caption}>Focuses on design excellence and strategic growth</p>         
         </Box>
     );
 
